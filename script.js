2d616fcad8ca02d27f25b607d23eef0271d133a9
Patrick Hiniker <hini0027@umn.edu>
2016-09-29 11:42:10 -0500

    created sell button, omitted start button

--- script.js
+++ script.js
@@ -1,28 +1,112 @@
 $( document ).ready(function() {
 
+	$('#SellOranges').click(function sOrange() {
 
-var count = 300;
-var counter = setInterval(timer, 1000);
+ 	 totalCash = totalCash + orangeMP;
+	 if (orangeCart < 1) {
+		 sOrange.stop();
+	 }
+	 orangeCart--;
+ 	 $('#totalCash').text('Total Cash: $' + totalCash.toFixed(2));
+ 	 $('#OrangeCart').text(orangeCart);
+  });
 
-function timer() {
-	count = count-1;
-	if (count <=0) {
-		clearInterval(counter);
-		return;
+	var averageOrange = 0;
+	var averageBanana = 0;
+	var averageGrape = 0;
+	var averageApple = 0;
+
+	var cart = 0;
+	var totalCash = 100;
+
+	var orangeMP = (randomNumber(50, 999)) / 100;
+	var bananaMP = (randomNumber(50, 999)) / 100;
+	var grapeMP = (randomNumber(50, 999)) / 100;
+	var appleMP = (randomNumber(50, 999)) / 100;
+
+// $('#start').click(function() {
+
+
+	// random initial market prices
+	function randomNumber(min, max) {
+		return Math.floor(Math.random() * (max - min) + min);
 	}
-	document.getElementById('timer').innerHTML=count + ' secs';
+
+
+
+//write market prices
+$('#MarketOrange').text(orangeMP.toFixed(2));
+$('#MarketBanana').text(bananaMP.toFixed(2));
+$('#MarketGrape').text(grapeMP.toFixed(2));
+$('#MarketApple').text(appleMP.toFixed(2));
+
+//write average prices
+$('#AverageOrange').text(averageOrange.toFixed(2));
+$('#AverageBanana').text(averageBanana.toFixed(2));
+$('#AverageGrape').text(averageGrape.toFixed(2));
+$('#AverageApple').text(averageApple.toFixed(2));
+
+$('#totalCash').text('Total Cash: $' + totalCash.toFixed(2));
+
+
+
+	var count = 300;
+	var counter = setInterval(timer, 1000);
+
+	function timer() {
+		count = count-1;
+		if (count <= -1) {
+			clearInterval(counter);
+			return;
+		}
+		document.getElementById('timer').innerHTML=count + ' seconds';
+	}
+
+
+	//  var addSub = 0;
+	//  var inc = 0;
+	//
+	//  function fifteenAddSub(min, max) {
+	//  	return Math.floor(Math.random() * (max - min) + min);
+	//  }
+	//
+	//  function fifteenInc(min, max) {
+	//  	return Math.floor(Math.random() * (max - min) + min);
+	//  }
+
+function doThis() {
+
+
 }
+
+	// // 15 second timer
+	// setInterval(function(fifteenOrange) {
+	//  $(orangeMP).doThis().overAndOver();
+	// }, 15000);
+	// setInterval(function(fifteenBananas) {
+	//  $(bananaMP).doThis().overAndOver();
+	// }, 15000);
+	// setInterval(function(fifteenGrapes) {
+	//  $(grapeMP).doThis().overAndOver();
+	// }, 15000);
+	// setInterval(function(fifteenApples) {
+	//  $(appleMP).doThis().overAndOver();
+	// }, 15000);
+
+
+
+// })
+
+
+
+
 
 var orangeCart = 0;
 var bananaCart = 0;
 var grapeCart = 0;
 var appleCart = 0;
 
-//num.toFixed(2)
-var averageOrange = 0;
-var averageBanana = 0;
-var averageGrape = 0;
-var averageApple = 0;
+
 
 var orangeSpent = 0;
 var bananaSpent = 0;
@@ -45,20 +129,13 @@ var appleClicks = 0;
 
 
 
-// random initial market prices
-function randomNumber(min, max) {
-	return Math.floor(Math.random() * (max - min) + min);
-}
 
-var orangeMP = (randomNumber(50, 999)) / 100;
-var bananaMP = (randomNumber(50, 999)) / 100;
-var grapeMP = (randomNumber(50, 999)) / 100;
-var appleMP = (randomNumber(50, 999)) / 100;
+
+
 
 // var fruits = [oranges, bananas, grapes, apples];
 
-var cart = 0;
-var totalCash = 100
+
 
 // //function for data storage
 // 	function buyFruit (object) {
@@ -77,12 +154,21 @@ var totalCash = 100
 
 
 
- $('#BuyOranges').click(function() {
+
+ $('#BuyOranges').click(function oClick() {
+
 	 orangeClicks++;
 	 orangeCart++;
 	 orangeSpent = orangeSpent + orangeMP;
 	 averageOrange = orangeSpent / orangeClicks;
-	 console.log(averageOrange);
+	 totalCash = totalCash - orangeMP;
+	 if (totalCash <0) {
+	 	oClick.stop();
+	 }
+	 //.off
+	 $('#totalCash').text('Total Cash: $' + totalCash.toFixed(2));
+	 $('#OrangeCart').text(orangeCart);
+	 $('#AverageOrange').text(averageOrange.toFixed(2));
  });
 
  $('#BuyBananas').click(function() {
@@ -108,6 +194,9 @@ var totalCash = 100
 	 averageApple = appleSpent / appleClicks;
 	 console.log(averageApple)
  });
+
+
+
 
 
 
